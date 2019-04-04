3a0c482a33340b4db37e75f1f85bf630d0e9c125
Katana Tran <tkatyk21@gmail.com>
2019-04-04 18:04:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,9 @@
 
 def speak_to_grandma(speak)
   if speak == speak.upcase
-    puts 
+    puts "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else 
-    puts "NO, NOT SINCE 1938!"
-    "HUH?! SPEAK UP, SONNY!"
+    puts "HUH?! SPEAK UP, SONNY!"
   
